0d55d213d456eb781c0e5e17494029d8ed485856
wipasirin <96044743+wipasirin@users.noreply.github.com>
2022-03-03 01:50:06 +0700

    Add files via upload

---
+++ projects/PyGame/AnimationEnemy.py
@@ -0,0 +1,54 @@
+import pygame
+import sys
+
+WIDTH = 750
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+clock = pygame.time.Clock()
+
+class animateEnemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.imgEne =[]
+        self.imgEne.append(pygame.image.load("projects\PyGame\pictures\ene1.png"))
+        self.imgEne.append(pygame.image.load("projects\PyGame\pictures\ene2.png"))
+        self.imgEne.append(pygame.image.load("projects\PyGame\pictures\ene3.png"))
+        self.imgEne.append(pygame.image.load("projects\PyGame\pictures\ene4.png"))
+        self.current_imgEne = 0
+        self.death_animation = False
+        self.image = self.imgEne[self.current_imgEne]
+        # self.char0_1 = pygame.transform.scale(char0 (int(char0.get_width() * scale, char0.get_height() * scale)))
+        
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x,y]
+    
+    def death(self):
+        self.death_animation = True
+
+    def update(self, speed): # speed ต้องบวกได้จำนวนเต็ม 0++
+        if self.death_animation == True:
+            self.current_imgEne += speed
+            if int(self.current_imgEne) >= len(self.imgEne):
+                self.current_imgEne = 0
+                self.death_animation = False
+        
+        self.image = self.imgEne[int(self.current_imgEne)]
+
+running = True
+animation_enemy = pygame.sprite.Group()
+timmie = animateEnemy(200, 200)
+animation_enemy.add(timmie)
+        
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            timmie.death()
+        
+    screen.fill((0,0,0))
+    animation_enemy.draw(screen) # draw charactor
+    animation_enemy.update(0.2)
+    pygame.display.update()
+    clock.tick(15)
